d68dd95a01bb36467b18e201f2561e2a22593736
Will1405 <willlevine1405@gmail.com>
2020-04-07 17:31:33 +0100

    added seperate pages

---
+++ AppWithBtns/App/ExampleHackApp/ExampleHackApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExampleHackApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void GiveHelp_Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+        void GetHelp_Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+
+        void MyMessages_Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+
+        void MyCard_Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Page4());
+        }
+    }
+}
