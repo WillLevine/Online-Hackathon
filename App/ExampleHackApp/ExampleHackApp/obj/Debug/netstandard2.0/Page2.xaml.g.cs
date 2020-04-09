e5c1f61066f5931200618d7b6f289ec1c8c1fa18
JennieCohen <JennieCohen@users.noreply.github.com>
2020-04-09 16:42:49 +0100

    place

--- App/ExampleHackApp/ExampleHackApp/obj/Debug/netstandard2.0/Page2.xaml.g.cs
+++ App/ExampleHackApp/ExampleHackApp/obj/Debug/netstandard2.0/Page2.xaml.g.cs
@@ -17,8 +17,12 @@ namespace ExampleHackApp {
     public partial class Page2 : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Maps.Map map;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Page2));
+            map = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Maps.Map>(this, "map");
         }
     }
 }
